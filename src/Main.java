b0df997cd989926415f5297e78f3799e12a7f79d
BT_2N2_18 <sample@bths.edu>
2023-12-19 13:28:32 -0500

    edited line 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("i love java!");
-        System.out.println("hiihiii");
+        System.out.println("wooooo");
         System.out.println("hallo!");
         System.out.println("yippeee");
         System.out.println("#codinggirlies");
