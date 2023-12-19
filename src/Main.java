179902e8328a2cd82ef1c25c457725d4f3b99f85
BT_2N2_18 <sample@bths.edu>
2023-12-19 13:26:31 -0500

    edited the first print statement

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("i love java!");
         System.out.println("hiihiii");
         System.out.println("hallo!");
         System.out.println("woahhhh");
