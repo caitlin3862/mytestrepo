6e7977322f5a38228ea6ae33c7a0ca0361764ca0
BT_2N2_17 <sample@bths.edu>
2023-12-19 13:28:24 -0500

    changed line 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("i love java!");
-        System.out.println("hiihiii");
+        System.out.println("hellohellohello");
         System.out.println("hallo!");
         System.out.println("yippeee");
         System.out.println("#codinggirlies");
